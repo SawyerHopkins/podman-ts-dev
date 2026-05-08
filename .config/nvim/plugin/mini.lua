3c2712a827d5564e976cea6bb5c7161b3aa87813
Sawyer Hopkins <sawyer@sawyerhopkins.com>
2026-05-08 06:32:47 +0000

    add status line hook inside mini setup

--- .config/nvim/plugin/mini.lua
+++ .config/nvim/plugin/mini.lua
@@ -90,3 +90,52 @@ vim.api.nvim_create_autocmd("User", {
     nvim_tree_api.tree.close()
   end,
 })
+
+--[[
+  Set status line filename highlight color when buffer is dirty 
+--]]
+
+local default_hl = nil -- Default highlight color
+local dirty_hl = nil -- Highlight color to use when file is dirty
+local target_hl = 'MiniStatuslineFilename' -- Highlight to change when dirty
+
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = function ()
+    default_hl = vim.api.nvim_get_hl_by_name(target_hl, true) -- Get the original target highlight
+    dirty_hl = vim.api.nvim_get_hl_by_name('WarningMsg', true)
+  end
+})
+
+vim.api.nvim_create_autocmd({
+  'BufEnter',
+  'BufModifiedSet',
+  'WinEnter',
+  'BufWritePost'
+}, {
+  callback = function()
+    -- Color scheme needs to be loaded to continue
+    if (default_hl == nil) or (dirty_hl == nil) then
+      return
+    end
+
+    if vim.bo.modified then
+      -- Set warning highlight
+      vim.api.nvim_set_hl(0, target_hl, {
+        fg = dirty_hl.foreground,
+        bg = dirty_hl.background,
+        bold = true
+      })
+    else
+      -- Restore default highlight
+      vim.api.nvim_set_hl(0, target_hl, {
+        fg = default_hl.foreground,
+        bg = default_hl.background,
+        bold = default_hl.bold,
+      })
+    end
+  end,
+})
+
+vim.cmd.colorscheme('catppuccin')
+
